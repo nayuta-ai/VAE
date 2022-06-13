c3983b2389b4a57d88dc784e0263ba7cc8aa89ad
Yuta Nagai <yuta@volupta.murata.eb.waseda.ac.jp>
2022-06-13 15:31:02 +0900

    Modify rubber_data_2022_n

--- config/path.py
+++ config/path.py
@@ -5,4 +5,4 @@ from os.path import join, dirname, realpath
 PROJECT_ROOT = join(sep, *dirname(realpath(__file__)).split(sep)[: -1])
 
 DATA_PATH = join(
-    PROJECT_ROOT, "data", "rubber", "rubber_data_2022", "*.pt")
+    PROJECT_ROOT, "data", "rubber", "rubber_data_2022_n", "*.pt")
